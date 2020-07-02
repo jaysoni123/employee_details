a1b6e2c42ae3349339b366395b837c9399c9ae79
Dhananjay Choksi <soni.jay008@gmail.com>
2020-07-02 20:02:07 +0530

    Updates

---
+++ precache-manifest.859bda8bf764246e6572fb51075f4d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809483957a92228ea24724a3b5cd14a7",
+    "url": "/employee_details/index.html"
+  },
+  {
+    "revision": "ad1e8b724c56f4a5aaba",
+    "url": "/employee_details/static/css/main.c0d344cf.chunk.css"
+  },
+  {
+    "revision": "151edc03013b157876b3",
+    "url": "/employee_details/static/js/2.2b4a2216.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee_details/static/js/2.2b4a2216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1e8b724c56f4a5aaba",
+    "url": "/employee_details/static/js/main.6e69f557.chunk.js"
+  },
+  {
+    "revision": "bf7a00de87c503dba354",
+    "url": "/employee_details/static/js/runtime-main.8abf74d7.js"
+  }
+]);
